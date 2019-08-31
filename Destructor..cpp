c26883329515c0cc6f08ed6337b3945ae0275e63
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2019-08-31 12:11:46 +0530

    destructor

---
+++ Destructor..cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+#include<conio.h>
+class sample{
+	private:
+	       int num;
+	
+	public:
+	      sample(int x)
+		  {
+		  	num = x;
+		  } 
+		  ~sample()
+		  {
+		   
+		   }
+		   
+		   
+		void display()
+		{
+			cout<<num;
+			  }      
+};
+main()
+{
+	sample s(20);
+	s.display();
+	return 0;
+}
